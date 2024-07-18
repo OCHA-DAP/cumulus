9e74e5fbe94eb7b52dcf02ed0c2a61fdc8e062e2
Zack <zackarno@gmail.com>
2024-07-18 10:46:39 -0400

    rm rlang::arg_match() from load_containers as it is handled by azure_endpoint_url()

--- R/load_containers.R
+++ R/load_containers.R
@@ -12,31 +12,25 @@
 #' @param storage_account Storage account to access. Default is `imb0chd0`
 #' @examples
 #' # load project containers
-#' pc <- load_containers(containers= "projects")
+#' pc <- load_containers(containers = "projects")
 #' AzureStor::list_blobs(
 #'   container = pc$PROJECTS_CONT,
 #'   dir = "ds-contingency-pak-floods"
 #' )
 #'
 #' # You can also list as many containers as you want.
-#' pc <- load_containers(containers= c("global","projects"))
+#' pc <- load_containers(containers = c("global", "projects"))
 #' AzureStor::list_blobs(
 #'   container = pc$GLOBAL_CONT,
 #'   dir = "raster/cogs"
 #' )
 #' @export
 load_containers <- function(
-    containers = c("global","projects"),
-    sas_key= Sys.getenv("DSCI_AZ_SAS_DEV"),
+    containers = c("global", "projects"),
+    sas_key = Sys.getenv("DSCI_AZ_SAS_DEV"),
     service = c("blob", "file"),
     stage = c("dev", "prod"),
-    storage_account = "imb0chd0"
-){
-
-  service <- rlang::arg_match(service)
-  stage <- rlang::arg_match(stage)
-  storae_account <- rlang::arg_match(storage_account)
-
+    storage_account = "imb0chd0") {
   ep_url <- azure_endpoint_url(
     service = service,
     stage = stage,
@@ -46,14 +40,13 @@ load_containers <- function(
   se <- AzureStor::storage_endpoint(ep_url, sas = sas_key)
 
   # storage container
-
-  item_labels <- paste0(toupper(containers),"_CONT")
+  item_labels <- paste0(toupper(containers), "_CONT")
   containers <- rlang:::set_names(containers, item_labels)
 
-  l_containers<- purrr::map(containers, \(container_name){
+  l_containers <- purrr::map(containers, \(container_name){
     AzureStor::storage_container(se, container_name)
-
   })
+
   l_containers
 }
 
@@ -68,9 +61,7 @@ load_containers <- function(
 azure_endpoint_url <- function(
     service = c("blob", "file"),
     stage = c("dev", "prod"),
-    storage_account = "imb0chd0"
-    ) {
-
+    storage_account = "imb0chd0") {
   blob_url <- "https://{storage_account}{stage}.{service}.core.windows.net/"
   service <- rlang::arg_match(service)
   stage <- rlang::arg_match(stage)
@@ -78,5 +69,3 @@ azure_endpoint_url <- function(
   endpoint <- glue::glue(blob_url)
   return(endpoint)
 }
-
-
