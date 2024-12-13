24a75caaa0bef0093efa6d901a161b36b3d85e84
Zack <zackarno@gmail.com>
2024-12-13 14:10:48 -0500

    insert progress_show param to *_read()/write() funcs

--- R/blob_write.R
+++ R/blob_write.R
@@ -12,18 +12,18 @@
 #' * GeoJSON: [sf::st_write()]
 #' * JSON: [jsonlite::write_json()]
 #'
-#' If `hs_local()`, the file is not uploaded to the container.
 #'
 #' @param df Data frame or simple features to save out.
 #' @param name Name of the file to write, including prefix (`input/` or `output/`)
 #'     and filetype `.parquet`.
 #' @param stage Store to access, either `dev` (default) or `prod`.
 #' @param container Container name (`character`) or actual container class object to read from
+#' @param progress_show show progress bar (`logical`) (default = TRUE)
 #'
 #' @returns Nothing, but file is written to selected container
 #'
 #' @export
-blob_write <- function(df, name, stage = c("dev","prod"), container = "projects") {
+blob_write <- function(df, name, stage = c("dev","prod"), container = "projects", progress_show = TRUE) {
   stage <- rlang::arg_match(stage)
   if(inherits(container, "character")){
     container <- blob_containers(stage = stage)[[container]]
@@ -32,22 +32,19 @@ blob_write <- function(df, name, stage = c("dev","prod"), container = "projects"
   fileext <- tools::file_ext(name)
   tf <- tempfile(fileext = paste0(".", fileext))
 
+
   switch(fileext,
          csv = readr::write_csv(x = df, file = tf, na = ""),
          parquet = arrow::write_parquet(x = df, sink = tf),
          json = jsonlite::write_json(x = df, path = tf),
          geojson = sf::st_write(obj = df, dsn = tf, quiet = TRUE)
   )
+  opts <- options(azure_storage_progress_bar = progress_show)
+  on.exit(options(opts))
 
-
-  # wrapping to suppress printing of progress bar
-  invisible(
-    utils::capture.output(
-      AzureStor::upload_blob(
-        container = container,
-        src = tf,
-        dest = name
-      )
-    )
+  AzureStor::upload_blob(
+    container = container,
+    src = tf,
+    dest = name
   )
 }
