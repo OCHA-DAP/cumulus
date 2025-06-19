bbe2688a6e921bc94b16faf4c84a7681f67886f3
Zack <zackarno@gmail.com>
2025-06-19 16:49:34 -0400

    utility func to read FAO GIEW ASI admin 1 level data as data.frame

---
+++ R/fao_asi_tabular.R
@@ -0,0 +1,19 @@
+
+
+#' get tabular ASI FAO data at admin 1 level by country
+#'
+#' @param iso3 `character` containing country iso3 code
+#'
+#' @returns `data.frame` containing contents of FAO admin 1 csv from GIEWS
+#' @export
+#'
+#' @examples
+#' df_syria <- fao_asi_adm1_tabular("SYR")
+fao_asi_adm1_tabular <- function(iso3){
+  iso3_upper <- toupper(iso3)
+    readr::read_csv(
+      glue::glue(
+        "https://www.fao.org/giews/earthobservation/asis/data/country/{iso3_upper}/MAP_ASI/DATA/ASI_Dekad_Season1_data.csv"),
+      show_col_types = FALSE
+      )
+}
